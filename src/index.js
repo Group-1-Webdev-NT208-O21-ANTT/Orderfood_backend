a54879086fc3db4efab6a40134a970bb92f4d08b
Carliscoding <101809224+Carliscoding@users.noreply.github.com>
2024-03-24 19:26:20 +0700

    setup_backendServer_fin.

---
+++ src/index.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const dotenv = require('dotenv');
+
+dotenv.config();
+
+const app = express();
+const port = process.env.PORT || 3005;
+
+app.get('/', (req, res) => {
+    res.send('Hello World of backend.');
+});
+
+app.listen(port, () => {
+    console.log('Server is running in port: ', port);
+});
